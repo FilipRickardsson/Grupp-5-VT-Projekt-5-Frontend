c3fb0af8e48efab5ce7df2d8fb34425f7c97f487
FilipRickardsson <filip.rickardsson.90@gmail.com>
2017-05-25 22:32:35 +0200

    Logic logic

--- QuizzToolFrontend/src/servercommunication/ServerConnection.java
+++ QuizzToolFrontend/src/servercommunication/ServerConnection.java
@@ -1,8 +1,34 @@
 package servercommunication;
 
-public class ServerCommunication {
-    
-    // Hello, Server?
-    // Yes, this is server... go away
-    
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import models.QuizzUser;
+
+public class ServerConnection {
+
+    private static ServerConnection serverConnection;
+    private Client client;
+
+    private ServerConnection() {
+        client = ClientBuilder.newClient();
+    }
+
+    public static ServerConnection getServerConnection() {
+        if (serverConnection == null) {
+            serverConnection = new ServerConnection();
+        }
+
+        return serverConnection;
+    }
+
+    public synchronized QuizzUser getQuizzUser(String username, String password) {
+        QuizzUser quizzUser = client.target("http://localhost:8080/QuizzToolBackend/webapi/")
+                .path("quizz_users/" + username + "&" + password)
+                .request(MediaType.APPLICATION_JSON)
+                .get(QuizzUser.class);
+
+        return quizzUser;
+    }
+
 }
