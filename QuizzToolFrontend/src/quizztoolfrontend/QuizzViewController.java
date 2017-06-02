632637e80b5d9d8dd53f6d3b55867921772e51c5
Fatemeh Farmahini <m_farmahini@yahoo.com>
2017-06-02 11:11:26 +0200

    create add quizz

--- QuizzToolFrontend/src/quizztoolfrontend/QuizzViewController.java
+++ QuizzToolFrontend/src/quizztoolfrontend/QuizzViewController.java
@@ -2,13 +2,25 @@ package quizztoolfrontend;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import models.Question;
 import servercommunication.ServerConnection;
 
 public class QuizzViewController implements Initializable {
+
+    @FXML
+    private TextField textField_QuizzName;
+    @FXML
+    private TextField textField_StartTime;
+    @FXML
+    private TextField textField_endTime;
+    @FXML
+    private DatePicker datePicker_date;
 
     //@FXML
     //private Label lbl_question;
@@ -19,6 +31,14 @@ public class QuizzViewController implements Initializable {
 //        Question question =   serverConnection.getQuestion(1);
   //      lbl_question.setText(question.getText());
     }    
+
+    @FXML
+    private void handlerBtnCreate(ActionEvent event) {
+    }
+
+    @FXML
+    private void handlerBtnCancel(ActionEvent event) {
+    }
     
     
 }
